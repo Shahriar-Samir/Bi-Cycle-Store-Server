7faf05e6dafa4281f60c3da3135b31c9c93cee0b
Shahriar-Samir <shabusiness035@gmail.com>
2024-11-23 21:33:43 +0600

    add: essential config files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,21 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  eslintPluginPrettier.configs.recommended,
   {
     rules: {
-      "no-unused-vars": "error",
-      "prefer-const": "error",
-      "no-console": "warn",
+      'no-unused-vars': 'error',
+      'prefer-const': 'error',
+      'no-console': 'warn',
     },
-    ignores: [".node_modules/*"],
+    ignores: ['.node_modules/*'],
   },
 ];
