6a3eb2b70890a37a132ca4f8cc60740dca3c3127
Shahriar-Samir <shabusiness035@gmail.com>
2024-11-23 22:22:02 +0600

    add: order module files

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,15 @@
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 const app: Application = express();
 
 // middleware configs
 app.use(express.json());
 app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  res.json({
+    message: 'Bi-Cycle-Store homepage',
+  });
+});
 
 export default app;
